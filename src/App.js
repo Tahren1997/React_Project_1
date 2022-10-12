363d3e4f64f824f672f9f16724ed7ec280c24e3f
Tyler Chen <tchen22618@outlook.com>
2022-10-12 23:30:20 +0800

    navbar project complete but cannot show

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import Counter from "./components/Counter"
-import MealsList from "./components/MealsList"
-import MealsProvider from "./providers/MealsProvider"
+import "./App.css";
+import Homepage from "./Homepage";
+import AboutLittleLemon from "./AboutLittleLemon";
+import Contact from "./Contact";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <MealsProvider>
-          <MealsList />
-          <Counter />
-      </MealsProvider>
+    <div> 
+	  <nav>
+      <Link to="/" className="nav-item">Homepage</Link>
+      <Link to="/about" className="nav-item">About Little Lemon</Link>
+      <Link to="/contact" className="nav-item">Contact</Link>
+	  </nav>
+      <Routes> 
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<AboutLittleLemon />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
